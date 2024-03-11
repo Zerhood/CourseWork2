2c5bd92a0163aa80d5e674a1a6c6155d05f166c0
Zerhood <Zerhood111@ya.ru>
2024-03-11 21:38:36 +0700

    init commit

---
+++ src/main/java/SkyPro/CourseWork2Application.java
@@ -0,0 +1,13 @@
+package SkyPro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseWork2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseWork2Application.class, args);
+	}
+
+}
---
+++ src/test/java/SkyPro/CourseWork2ApplicationTests.java
@@ -0,0 +1,13 @@
+package SkyPro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseWork2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
